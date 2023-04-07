26d280f911187c8a50412cc579a50a4f8c94648a
Zzfathir <ahmadfathir0830@gmail.com>
2023-04-07 10:20:41 +0700

    set seeder

---
+++ database/seeders/CitySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\City;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $fileCity = file_get_contents(base_path('\database\city.json'));
+        $fileRegency = file_get_contents(base_path('\database\regency.json'));
+
+        $dataCity = json_decode($fileCity, true);
+        $dataRegency = json_decode($fileRegency, true);
+
+        City::insert($dataCity);
+        City::insert($dataRegency);
+    }
+}
